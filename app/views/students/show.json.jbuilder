1657f6327652e19901528595eca3c8720c906e70
Roberto Claure <robertoclaure@gmail.com>
2013-08-01 22:50:50 -0500

    Add Bootstrap

---
+++ app/views/students/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @student, :name, :course_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 StudentClasses::Application.routes.draw do
+  resources :courses
+
+  resources :students
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
